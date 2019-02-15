e81c96418f92a8d355a3b11365456ec82ae7a262
mohican19 <alban.belin@gmail.com>
2019-02-15 12:21:51 +0100

    rectification du head (variable  autre) dans le gabarit

--- app/vue/gabarit.php
+++ app/vue/gabarit.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>TechinSport RGPD</title>
+        <title><?= $title ?></title>
         <meta charset="utf-8">
       	<meta http-equiv="X-UA-Compatible" content="IE=edge">
       	<meta name="viewport" content="width=device-width, initial-scale=1">
-      	<title>B3D</title>
-      	<link href="Public/libs/bootstrap-4.1/css/bootstrap.min.css" rel="stylesheet">
-      	<script src="Public/libs/bootstrap-4.1/js/bootstrap.min.js"></script>
-      	<script src="Public/scripts/jquery-3.3.1.min.js"></script>
-      	<script src="Public/libs/bootstrap-4.1/js/bootstrap.bundle.min.js"></script>
-      	<link href="Public/libs/fontawesome-free-5.5.0-web/css/all.css" rel="stylesheet">
-      	<script defer src="Public/libs/fontawesome-free-5.5.0-web/js/all.js"></script>
-        <link rel="stylesheet" href="Public/styles.css">
+      	<link href="lib/bootstrap-4.1/css/bootstrap.min.css" rel="stylesheet">
+      	<script src="lib/bootstrap-4.1/js/bootstrap.min.js"></script>
+      	<script src="lib/jquery-3.3.1.min.js"></script>
+      	<script src="lib/bootstrap-4.1/js/bootstrap.bundle.min.js"></script>
+      	<link href="lib/fontawesome-free-5.5.0-web/css/all.css" rel="stylesheet">
+      	<script defer src="lib/fontawesome-free-5.5.0-web/js/all.js"></script>
+        <link rel="stylesheet" href="Css/master.css">
     </head>
 
     <body>
