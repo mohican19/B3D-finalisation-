9d634525f86eaa407e8b64c2a87633056f55acc3
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-02-15 15:27:56 +0100

    Routeur.php_01

--- app/controleur/Router.php
+++ app/controleur/Router.php
@@ -4,22 +4,49 @@ namespace app\controleur;
 use \app\Database;
 use \app\vue\Vue;
 
-/**
- *
- */
-class Router
-{
-    private $action;
-    public function __construct()
-    {
-        if (isset($_GET['action'])) {
-            $this->action = $_GET['action'];
+class Routeur {
+    private $ctrlAccueil;
+    private $ctrlSociete;
+    private $ctrlProduit;
+    private $ctrlContact;
+
+    public function __construct() {
+        $this->ctrlAccueil = new ControleurAccueil();
+        $this->ctrlSociete = new ControleurSociete();
+        $this->ctrlProduit = new ControleurProduit();
+        $this->ctrlContact = new ControleurContact();
+    }
+
+    // Route une requête entrante : exécution l'action associée
+    public function requestRouting() {
+        try {
+            if (isset($_GET['action'])) {
+                if ($_GET['action'] == 'societe') {
+                    $this->ctrlSociete->societe();
+                  }elseif ($_GET['action'] == 'contact') {
+                        $this->ctrlContact->contact);
+                      }elseif ($_GET['action'] == 'produit') {
+                            $idProduit = intval($_GET['id']);
+                            if ($idProduit != 0) {
+                                $this->ctrlProduit->produit($idProduit);
+                  }
+                  else {
+                        throw new Exception("Identifiant de page non valide");
+              }
+              else {
+                    throw new Exception("Action non valide");
+            }
+            else {  // aucune action définie : affichage de l'accueil
+                $this->ctrlAccueil->accueil();
+            }
+        }
+        catch (Exception $e) {
+            $this->erreur($e->getMessage());
         }
     }
-    public function requestRouting()
-    {
+
+    // Affiche une erreur
+    private function erreur($msgErreur) {
+        $vue = new Vue("Erreur");
+        $vue->generer(array('msgErreur' => $msgErreur));
     }
-    private function error($error)
-    {
-    }
-}
