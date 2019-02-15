906a94d1050a4fedeb75f4d594bd5fcd6a15c6c8
Jarod <jarod.midy@gmail.com>
2019-02-15 14:22:18 +0100

    vue langue

---
+++ app/vue/templates/langues.php
@@ -0,0 +1,17 @@
+	<div id="choix-langue" class="container-fluid p-5 justify-content-center align-items-center">
+	<div>
+		<div class="logo">
+			<img src="images/logo_blanc_langue.png" alt="Logo B3D" class="mx-auto d-block img-fluid">
+		</div>
+		<form id="form-choix-langue" method="post" class="btnChoix row">
+			<input id="input-langue" class="d-none" type="text" name="langue" value="">
+			<button value="Francais" type="submit" class="btn scroll-langue">FRANÇAIS</button>
+			<p class="btnsep">|</p>
+			<button value="Anglais" type="submit" class="btn scroll-langue">ENGLISH</button>
+			<p class="btnsep">|</p>
+			<button value="Allemand" type="submit" class="btn scroll-langue">DEUTSCH</button>
+			<p class="btnsep">|</p>
+			<button value="Espagnol" type="submit" class="btn scroll-langue">ESPAÑOL</button>
+		</form>
+	</div>
+</div>
