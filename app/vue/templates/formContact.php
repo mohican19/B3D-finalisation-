be1be2ac50fae96eb4a924a0a393dade9ce2ba47
Jarod <jarod.midy@gmail.com>
2019-02-18 15:11:59 +0100

    underscore en tiret

--- app/vue/templates/formContact.php
+++ app/vue/templates/formContact.php
@@ -9,26 +9,26 @@ $submit = 'Envoyer';
  ?>
 
 
-  <form id="form-contact" action="" method="POST" accept-charset="utf-8" class="row">
+  <form id="form-contact" method="POST" accept-charset="utf-8" class="row">
 
-    <div class="col-lg-2 input_otst"></div>
+    <div class="col-lg-2 input-otst"></div>
 
-    <div class="col-lg-4  input_otst">
-      <label for="first_name"><?= $firstName ?> <span class="star">*</span> :</label>
+    <div class="col-lg-4  input-otst">
+      <label for="first-name"><?= $firstName ?> <span class="star">*</span> :</label>
       <div class="input-group">
         <span class="clearable">
-          <input type="text" class="form-control" id="first_name" name="first_name" aria-describedby="sizing-addon2"  placeholder="<?= $firstName ?>">
-          <i class="clearable__clear"><i class="fas fa-times-circle"></i></i>
+          <input type="text" class="form-control" id="first-name" name="first-name" aria-describedby="sizing-addon2"  placeholder="<?= $firstName ?>">
+          <i class="clearable-clear"><i class="fas fa-times-circle"></i></i>
         </span>
       </div>
     </div><!--==========-->
 
-    <div class="col-lg-4 input_otst">
-      <label for="last_name"><?= $name ?> <span class="star">*</span> :</label>
+    <div class="col-lg-4 input-otst">
+      <label for="last-name"><?= $name ?> <span class="star">*</span> :</label>
       <div class="input-group">
         <span class="clearable">
-          <input type="text" class="form-control" id="last_name" name="last_name" aria-describedby="sizing-addon2" placeholder="<?= $name ?>">
-          <i class="clearable__clear"><i class="fas fa-times-circle"></i></i>
+          <input type="text" class="form-control" id="last-name" name="last-name" aria-describedby="sizing-addon2" placeholder="<?= $name ?>">
+          <i class="clearable-clear"><i class="fas fa-times-circle"></i></i>
         </span>
       </div>
     </div><!--==========-->
@@ -37,22 +37,22 @@ $submit = 'Envoyer';
     <div class="col-lg-2"></div>
 
 
-    <div class="col-lg-4 input_otst">
+    <div class="col-lg-4 input-otst">
       <label for="email"><?= $email ?> <span class="star">*</span> :</label>
       <div class="input-group">
         <span class="clearable">
           <input type="text" class="form-control" id="email" name="email" aria-describedby="sizing-addon2" placeholder="<?= $email ?>">
-          <i class="clearable__clear"><i class="fas fa-times-circle"></i></i>
+          <i class="clearable-clear"><i class="fas fa-times-circle"></i></i>
         </span>
       </div>
     </div><!--==========-->
 
-    <div class="col-lg-4 input_otst">
+    <div class="col-lg-4 input-otst">
       <label for="phone"><?= $phone ?> :</label>
       <div class="input-group">
         <span class="clearable">
           <input type="text" class="form-control" id="phone" name="phone" aria-describedby="sizing-addon2" placeholder="<?= $phone ?>">
-          <i class="clearable__clear"><i class="fas fa-times-circle"></i></i>
+          <i class="clearable-clear"><i class="fas fa-times-circle"></i></i>
         </span>
       </div>
     </div><!--==========-->
@@ -69,7 +69,7 @@ $submit = 'Envoyer';
     <div class="col-lg-2"></div>
 
     <div class="col-lg-8">
-      <div class="input_otst"><span class="star">*</span><?= $rgpd ?></div>
+      <div class="input-otst"><span class="star">*</span><?= $rgpd ?></div>
       <div class="form-group form-check">
         <input type="checkbox" class="form-check-input" id="check-rgpd" name="check-rgpd">
         <label class="form-check-label" for="check-rgpd"><?= $rgpd ?> <span class="star">*</span></label>
@@ -79,8 +79,8 @@ $submit = 'Envoyer';
     <div class="col-lg-2"></div>
     <div class="col-lg-2"></div>
 
-    <div class="col-lg-8 input_otst">
-      <input type='submit' name='submit' class="btn btn-primary col-sm col-lg-2 bot_envoye" value="<?= $submit ?>"></input>
+    <div class="col-lg-8 input-otst">
+      <input type='submit' name='submit' class="btn btn-primary col-sm col-lg-2 bot-envoye" value="<?= $submit ?>"></input>
     </div>
 
     <div class="col-lg-2"></div>
