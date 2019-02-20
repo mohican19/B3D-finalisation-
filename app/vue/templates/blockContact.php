aca7c910dd7b5b2a9e03cc5cdfa498908f9b3063
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-02-20 15:44:24 +0100

    Creation Légales.php et nettoyage blockContact.php
    Pas de css mais le contenu dans des variables

--- app/vue/templates/blockContact.php
+++ app/vue/templates/blockContact.php
@@ -15,47 +15,33 @@ $mail_titre = "Email";
 $mail = "julien.procedes@orange.fr";
 ?>
 
-<div class="cont1 col-md-12">
+<section>
 
-          <div class="row col-md-12">
-              <div class="container">
-              <div class="panel-white">
-              <div class="header" id="contact"><i class="far fa-envelope"></i> <?= $titre; ?></div>
-              <div class="content">
 
-                 <div class="row wo-marg">
-                    <div class="col-md-3 col-sm-6">
-                      <div class="contact-cont">
+              <div><?= $titre; ?></div>
+              <div>
+                      <div>
                         <h2><?= $adresse_titre; ?></h2>
-                        <div class="contact_text"><?= $adresse; ?></div>
+                        <div><?= $adresse; ?></div>
                       </div>
-
-                      <div class="contact-cont">
+                      <div>
                         <h2><?= $horaires_titre; ?></h2>
-                        <div class="contact_text"><?= $horaires; ?></div>
+                        <div><?= $horaires; ?></div>
                       </div>
-                    </div>
-
-                    <div class="col-md-3 col-sm-6">
-                      <div class="contact-cont">
-                        <h2><?= $tel_titre; ?></h2>
-                        <div class="contact_text"><?= $tel; ?></div>
-                      </div>
-
-                      <div class="contact-cont">
-                        <h2><?= $mail_titre; ?></h2>
-                        <div class="contact_text"><?= $mail; ?></div>
-                      </div>
-                    </div>
-
-                    <div class="col-md-6  col-sm-12">
-                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5706.59330503512!2d0.07091420449699201!3d44.344956780262315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aa7c208889a78f%3A0x8662b027363dce5a!2sL&#39;Intendant%2C+47700+Poussignac!5e0!3m2!1sfr!2sfr!4v1545297392869" width="100%" height="200" frameborder="0" style="border:1" allowfullscreen></iframe>
-                    </div>
-                  </div>
-
               </div>
-            </div>
-          </div>
 
-  </div>
-</div>
+              <div>
+                      <div>
+                        <h2><?= $tel_titre; ?></h2>
+                        <div><?= $tel; ?></div>
+                      </div
+                      <div>
+                        <h2><?= $mail_titre; ?></h2>
+                        <div><?= $mail; ?></div>
+                      </div>
+              </div>
+
+              <div>
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5706.59330503512!2d0.07091420449699201!3d44.344956780262315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aa7c208889a78f%3A0x8662b027363dce5a!2sL&#39;Intendant%2C+47700+Poussignac!5e0!3m2!1sfr!2sfr!4v1545297392869" width="100%" height="200" frameborder="0" style="border:1" allowfullscreen></iframe>
+              </div>
+</section>
---
+++ app/vue/templates/legales.php
@@ -0,0 +1,82 @@
+<?=
+$titrePage = "Conditions générales d'utilisation du site B3D";
+$titreArticle01 = "ARTICLE 1 : Objet";
+$article01 = "Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique de l’utilisation du site B3D et de ses services.<br>Ce contrat est conclu entre :<br>Le gérant du site internet, ci-après désigné « l’Éditeur »,<br>Toute personne physique ou morale souhaitant accéder au site et à ses services, ci-après appelé « l’Utilisateur ».<br>Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acceptation de ces conditions.";
+
+$titreArticle02 = "ARTICLE 2 : Mentions légales";
+$article02 = "Pour les personnes morales :<br><br>Le site B3D est édité par la société B3D, SARL à associé unique au capital de 10 000 €, dont le siège social est situé à L'intendant, 47700 Poussignac.<br>La société est représentée par Mr Procedes Julien, Marc.<br><br>Pour les personnes physiques : <br><br>Le site B3D est édité par la société B3D, SARL à associé unique au capital de 10 000 €, dont le siège social est situé à L'intendant, 47700 Poussignac.<br>La société est représentée par Mr Procedes Julien, Marc.";
+
+$titreArticle03 = "ARTICLE 3 : accès aux services";
+$article03 = "Tout Utilisateur ayant accès a internet peut accéder gratuitement et depuis n’importe où au site. Les frais supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la charge de l’Éditeur.<br>Le site et ses différents services peuvent être interrompus ou suspendus par l’Éditeur, notamment à l’occasion d’une maintenance, sans obligation de préavis ou de justification.";
+
+$titreArticle04 = "ARTICLE 4 : Responsabilité de l’Éditeur";
+$article04 = "Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité de l’Éditeur.<br>De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.<br>B3D s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.<br>L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations diffusées su le site soient réputées fiables.";
+
+$titreArticle05 = "ARTICLE 5 : Propriété intellectuelle";
+$article05 = "Les contenus du site B3D (logos, textes, éléments graphiques, vidéos, etc.) son protégés par le droit d’auteur, en vertu du Code de la propriété intellectuelle.<br>L’Utilisateur devra obtenir l’autorisation de l’éditeur du site avant toute reproduction, copie ou publication de ces différents contenus.<br>Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.";
+
+$titreArticle06 = "ARTICLE 6 : Données personnelles";
+$article06 = "L’Utilisateur doit obligatoirement fournir son adresse é-mail pour procéder à une demande de contact ou de renseignements. <br>L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site B3D pour la communication d’informations diverses et la gestion des commandes et des réponses par e-mail.<br>B3D garantie le respect de la vie privée de l’utilisateur, conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.<br>Le site est déclaré auprès de la CNIL sous le numéro suivant : XXXX.<br>En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. <br>L'Utilisateur exerce ce droit via :<br>+ Un formulaire de contact<br>+ Par voie postale à l'adresse :<br>B3D<br>L'intendant,<br>47700 Poussignac.";
+
+$titreArticle07 = "ARTICLE 7 : Liens hypertextes";
+$article07 = "Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité de l’Éditeur de B3D, qui n’a pas de contrôle sur ces liens.<br>Il est possible pour un tiers de créer un lien vers une page du site B3D sans autorisation expresse de l’éditeur.";
+
+$titreArticle08 = "ARTICLE 8 : Évolution des conditions générales d’utilisation";
+$article08 = "Le site B3D se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.";
+
+$titreArticle09 = "ARTICLE 9 : Droit applicable et juridiction compétente";
+$article09 = "Le présent contrat dépend de la législation française.<br>En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur, les tribunaux de Cahors sont compétents pour régler le contentieux.";
+
+?>
+
+<section>
+
+	<h1><?= $titrePage; ?></h1><br>
+
+	<article class="row"><br>
+	<h5><?= $titreArticle01; ?><hr></h5><br>
+	<p><?= $article01; ?></p>
+	</article>
+
+  <article class="row"><br>
+	<h5><?= $titreArticle02; ?><hr></h5><br>
+	<p><?= $article02; ?></p>
+	</article>
+
+	<article class="row"><br>
+	<h5><?= $titreArticle03; ?><hr></h5><br>
+	<p><?= $article03; ?></p>
+	</article>
+
+  <article class="row"><br>
+	<h5><?= $titreArticle04; ?><hr></h5><br>
+	<p><?= $article04; ?></p>
+	</article>
+
+	<article class="row"><br>
+	<h5><?= $titreArticle05; ?><hr></h5><br>
+	<p><?= $article05; ?></p>
+	</article>
+
+  <article class="row"><br>
+	<h5><?= $titreArticle06; ?><hr></h5><br>
+	<p><?= $article06; ?></p>
+	</article>
+
+	<article class="row"><br>
+	<h5><?= $titreArticle07; ?><hr></h5><br>
+	<p><?= $article07; ?></p>
+	</article>
+
+  <article class="row"><br>
+	<h5><?= $titreArticle08; ?><hr></h5><br>
+	<p><?= $article08; ?></p>
+	</article>
+
+  <article class="row"><br>
+	<h5><?= $titreArticle09; ?><hr></h5><br>
+	<p><?= $article09; ?></p>
+	</article>
+
+
+</section>
