47a115f5b67a8fdb0e448392d82edb5a0080ad9d
mohican19 <alban.belin@gmail.com>
2019-02-20 16:57:43 +0100

    mise en place du css pour le block contact

--- app/vue/templates/blockContact.php
+++ app/vue/templates/blockContact.php
@@ -1,9 +1,4 @@
-<link rel="stylesheet" href="./stylesheet/css/css a trier/blockContact.css">
-<link rel="stylesheet" href="./stylesheet/css/css a trier/produits.css">
-
-
-
-<?=
+<?php
 $titre = "Contact";
 $adresse_titre = "Adresse";
 $adresse = "B3D \"L'intendant\" <br>47700 Poussignac";
@@ -15,33 +10,32 @@ $mail_titre = "Email";
 $mail = "julien.procedes@orange.fr";
 ?>
 
-<section>
-
-
-              <div><?= $titre; ?></div>
-              <div>
-                      <div>
-                        <h2><?= $adresse_titre; ?></h2>
-                        <div><?= $adresse; ?></div>
-                      </div>
-                      <div>
-                        <h2><?= $horaires_titre; ?></h2>
-                        <div><?= $horaires; ?></div>
-                      </div>
+<section class="container bloc-texte mb-5">
+              <h3 class="titre-de-bloc"><?= $titre; ?></h3>
+              <div class="row contenu-bloc-texte">
+                <div class="col-md-3 ">
+                  <div>
+                    <h3><?= $adresse_titre; ?></h3>
+                    <div><?= $adresse; ?></div>
+                  </div>
+                  <div>
+                    <h3><?= $horaires_titre; ?></h3>
+                    <div><?= $horaires; ?></div>
+                  </div>
+                </div>
+                <div class="col-md-3 ">
+                  <div>
+                    <h3><?= $tel_titre; ?></h3>
+                    <div><?= $tel; ?></div>
+                  </div>
+                  <div>
+                    <h3><?= $mail_titre; ?></h3>
+                    <div><?= $mail; ?></div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5706.59330503512!2d0.07091420449699201!3d44.344956780262315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aa7c208889a78f%3A0x8662b027363dce5a!2sL&#39;Intendant%2C+47700+Poussignac!5e0!3m2!1sfr!2sfr!4v1545297392869" width="100%" height="200" frameborder="0" style="border:1" allowfullscreen></iframe>
+                </div>
               </div>
 
-              <div>
-                      <div>
-                        <h2><?= $tel_titre; ?></h2>
-                        <div><?= $tel; ?></div>
-                      </div
-                      <div>
-                        <h2><?= $mail_titre; ?></h2>
-                        <div><?= $mail; ?></div>
-                      </div>
-              </div>
-
-              <div>
-                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5706.59330503512!2d0.07091420449699201!3d44.344956780262315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aa7c208889a78f%3A0x8662b027363dce5a!2sL&#39;Intendant%2C+47700+Poussignac!5e0!3m2!1sfr!2sfr!4v1545297392869" width="100%" height="200" frameborder="0" style="border:1" allowfullscreen></iframe>
-              </div>
 </section>
