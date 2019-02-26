a3ea6a60f7a65a7f9283c9b3e42a2188ccec4231
mohican19 <alban.belin@gmail.com>
2019-02-26 11:11:42 +0100

    création de la vue produits (servira probablement de page d'accueil)

---
+++ app/vue/templates/produits.php
@@ -0,0 +1,11 @@
+<section id="produits" class="container">
+  <div id="" class="justify-content-around bloc1 bloc-texte pb-2">
+    <h3 class="titre-de-bloc">Piquets en châtaignier</h3>
+    <p>Nos piquets et rondins sont produits à partir de grumes exploitées dans un rayon de 70 km. Le pin provient de coupes d’éclaircies plantées après la tempête de 1999.</br></br>Ces bois juvéniles utilisés généralement pour la mise en place de filets anti-grêle ou le palissage comportent une très faible proportion de bois de cœur leurs donnant une très bonne aptitude au traitement autoclave.</br></br>Nos grumes d’acacia comportent une très faible quantité d’aubier et des cernes d’accroissement serrées conférant au bois une excellente durabilité naturelle et une très bonne résistance mécanique. L’ensemble de nos bois proviennent de coupes gérées durablement et bénéficie généralement de la certification PEFC.<p>
+  </div>
+
+
+
+
+
+</section>
