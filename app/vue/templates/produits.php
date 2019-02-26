ae3a82d8d6a8896904276683fdc8d464e9a49e2e
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-02-26 16:02:07 +0100

    Page produits et 001 002 003 004.jpg

--- app/vue/templates/produits.php
+++ app/vue/templates/produits.php
@@ -1,11 +1,43 @@
 <section id="produits" class="container">
   <div id="" class="justify-content-around bloc1 bloc-texte pb-2">
-    <h3 class="titre-de-bloc">Piquets en châtaignier</h3>
+    <h3 class="titre-de-bloc">Produits</h3>
+    <div class="mx-3">
     <p>Nos piquets et rondins sont produits à partir de grumes exploitées dans un rayon de 70 km. Le pin provient de coupes d’éclaircies plantées après la tempête de 1999.</br></br>Ces bois juvéniles utilisés généralement pour la mise en place de filets anti-grêle ou le palissage comportent une très faible proportion de bois de cœur leurs donnant une très bonne aptitude au traitement autoclave.</br></br>Nos grumes d’acacia comportent une très faible quantité d’aubier et des cernes d’accroissement serrées conférant au bois une excellente durabilité naturelle et une très bonne résistance mécanique. L’ensemble de nos bois proviennent de coupes gérées durablement et bénéficie généralement de la certification PEFC.<p>
+    </div>
   </div>
-
-
-
-
+      <div class="container d-flex mb-4">
+      <div id="" class="justify-content-around bloc-texte pb-2 col-md-3 m-1">
+        <h3 class="titre-de-bloc">Piquets Châtaignier</h3>
+          <div class="mx-2">
+          <img class="img-fluid" src="images/001.jpg">
+          <p>Essence imputrescible découvrez les spécifications de nos piquets.</p>
+          <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Châtaignier</a>
+          </div>
+      </div>
+      <div id="" class="justify-content-around bloc-texte pb-2 col-md-3 m-1">
+        <h3 class="titre-de-bloc">Piquets Acacia</h3>
+            <div class="mx-2">
+            <img class="img-fluid" src="images/002.jpg">
+            <p>Ecorchés pour apporter plus de propreté, découvrez l'acacia.</p>
+            <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Acacia</a>
+            </div>
+      </div>
+      <div id="" class="justify-content-around bloc-texte col-md-3 m-1">
+        <h3 class="titre-de-bloc">Piquets Pin</h3>
+            <div class="mx-2">
+            <img class="img-fluid" src="images/003.jpg">
+            <p>Nos piquets et poteaux pins sont planés. Découvrez-en plus.</p>
+            <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Pin</a>
+            </div>
+      </div>
+      <div id="" class="justify-content-around bloc-texte pb-2 col-md-3 m-1">
+        <h3 class="titre-de-bloc">Bois de chauffe</h3>
+            <div class="mx-2">
+            <img class="img-fluid" src="images/004.jpg">
+            <p>Notre bois de chauffage est issu d'acacia. Découvrez le !</p>
+            <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Bois de chauffe</a>
+            </div>
+      </div>
+  </div>
 
 </section>
