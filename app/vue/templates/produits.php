9648c6cdf1094376ca164e19e9bf8a92e55ceec4
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-02-26 16:09:44 +0100

    MAJ page produits.php

--- app/vue/templates/produits.php
+++ app/vue/templates/produits.php
@@ -5,7 +5,7 @@
     <p>Nos piquets et rondins sont produits à partir de grumes exploitées dans un rayon de 70 km. Le pin provient de coupes d’éclaircies plantées après la tempête de 1999.</br></br>Ces bois juvéniles utilisés généralement pour la mise en place de filets anti-grêle ou le palissage comportent une très faible proportion de bois de cœur leurs donnant une très bonne aptitude au traitement autoclave.</br></br>Nos grumes d’acacia comportent une très faible quantité d’aubier et des cernes d’accroissement serrées conférant au bois une excellente durabilité naturelle et une très bonne résistance mécanique. L’ensemble de nos bois proviennent de coupes gérées durablement et bénéficie généralement de la certification PEFC.<p>
     </div>
   </div>
-      <div class="container d-flex mb-4">
+      <div class="justify-content-around container d-flex mb-4">
       <div id="" class="justify-content-around bloc-texte pb-2 col-md-3 m-1">
         <h3 class="titre-de-bloc">Piquets Châtaignier</h3>
           <div class="mx-2">
