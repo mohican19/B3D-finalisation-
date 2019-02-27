26da8448a4390a5c081246df1ac54321d4c1f139
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-02-27 16:22:13 +0100

    Produits.php en dur, petite modif css master, ajout imgs slider

--- app/vue/templates/produits.php
+++ app/vue/templates/produits.php
@@ -5,39 +5,73 @@
     <p>Nos piquets et rondins sont produits à partir de grumes exploitées dans un rayon de 70 km. Le pin provient de coupes d’éclaircies plantées après la tempête de 1999.</br></br>Ces bois juvéniles utilisés généralement pour la mise en place de filets anti-grêle ou le palissage comportent une très faible proportion de bois de cœur leurs donnant une très bonne aptitude au traitement autoclave.</br></br>Nos grumes d’acacia comportent une très faible quantité d’aubier et des cernes d’accroissement serrées conférant au bois une excellente durabilité naturelle et une très bonne résistance mécanique. L’ensemble de nos bois proviennent de coupes gérées durablement et bénéficie généralement de la certification PEFC.<p>
     </div>
   </div>
-      <div class="justify-content-around container d-flex mb-4">
-      <div id="" class="justify-content-around bloc-texte pb-2 col-md-3 m-1">
+      <div class="container justify-content-around d-flex">
+      <div id="" class="card bloc-texte col-lg-3 pb-2 m-1">
         <h3 class="titre-de-bloc">Piquets Châtaignier</h3>
+        <img class="card-img-top" src="images/001.jpg" alt="Image Carte Chataignier">
           <div class="mx-2">
-          <img class="img-fluid" src="images/001.jpg">
           <p>Essence imputrescible découvrez les spécifications de nos piquets.</p>
           <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Châtaignier</a>
           </div>
       </div>
-      <div id="" class="justify-content-around bloc-texte pb-2 col-md-3 m-1">
+      <div id="" class="card bloc-texte col-lg-3 pb-2 m-1">
         <h3 class="titre-de-bloc">Piquets Acacia</h3>
+        <img class="card-img-top" src="images/001.jpg" alt="Image Carte Acacia">
             <div class="mx-2">
-            <img class="img-fluid" src="images/002.jpg">
             <p>Ecorchés pour apporter plus de propreté, découvrez l'acacia.</p>
             <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Acacia</a>
             </div>
       </div>
-      <div id="" class="justify-content-around bloc-texte col-md-3 m-1">
+      <div id="" class="card bloc-texte col-lg-3 pb-2 m-1">
         <h3 class="titre-de-bloc">Piquets Pin</h3>
+        <img class="card-img-top" src="images/001.jpg" alt="Image Carte Pin">
             <div class="mx-2">
-            <img class="img-fluid" src="images/003.jpg">
             <p>Nos piquets et poteaux pins sont planés. Découvrez-en plus.</p>
             <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Pin</a>
             </div>
       </div>
-      <div id="" class="justify-content-around bloc-texte pb-2 col-md-3 m-1">
+      <div id="" class="card bloc-texte col-lg-3 pb-2 m-1">
         <h3 class="titre-de-bloc">Bois de chauffe</h3>
+        <img class="card-img-top" src="images/001.jpg" alt="Image Carte Bois de chauffe">
             <div class="mx-2">
-            <img class="img-fluid" src="images/004.jpg">
             <p>Notre bois de chauffage est issu d'acacia. Découvrez le !</p>
             <a href="#" class="btn btn-block btn-sm btn-outline-danger">Nos Produits Bois de chauffe</a>
             </div>
       </div>
   </div>
+</section>
 
+<section class="container">
+  <div class="my-5 mx-1">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+      <div class="carousel-item active">
+      <img class="d-block w-100" src="images/Acacia1.jpg" alt="#">
+      </div>
+      <div class="carousel-item">
+      <img class="d-block w-100" src="images/tas1.jpg" alt="#">
+      </div>
+      <div class="carousel-item">
+      <img class="d-block w-100" src="images/Acacia3.jpg" alt="#">
+      </div>
+      <div class="carousel-item">
+      <img class="d-block w-100" src="images/Acacia7.jpg" alt="#">
+      </div>
+      <div class="carousel-item">
+      <img class="d-block w-100" src="images/tas2.jpg" alt="#">
+      </div>
+      <div class="carousel-item">
+      <img class="d-block w-100" src="images/tas3.jpg" alt="#">
+      </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+      </a>
+  </div>
+</div>
 </section>
