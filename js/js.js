52092434a34e2a5e247e996e7be55da0db9b6ee9
mohican19 <alban.belin@gmail.com>
2019-03-02 08:44:38 +0100

    modif de la donction de scroll-up et css associé

--- js/js.js
+++ js/js.js
@@ -4,9 +4,40 @@ $('#show-desc').on('click', function(){
         })
 
 // le scroll de la fléche du bas
-$(".scroll-up").click(function() {
+/*$(".scroll-up").click(function() {
   var section = $('#header');
   $("html,body").animate({
   scrollTop: section.offset().top
   }, "slow");
+});*/
+
+ScrollToTop=function() {
+  var s = $(window).scrollTop();
+  if (s > 80) {
+    $('.scroll-up').fadeIn();
+  } else {
+    $('.scroll-up').fadeOut();
+  }
+
+  $('.scroll-up').click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 500);
+      return false;
+  });
+
+  /*if ( s > 720) {
+    $('.scroll-up').css('bottom','200px');
+  } else {
+    $('.scroll-up').css('bottom','100px');
+  }*/
+}
+
+StopAnimation=function() {
+  $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
+    $('html, body').stop();
+  });
+}
+
+$(window).scroll(function() {
+  ScrollToTop();
+  StopAnimation();
 });
