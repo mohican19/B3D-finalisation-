18ca6001f142119fa6513047beda140d9a196dc5
mohican19 <alban.belin@gmail.com>
2019-03-02 09:39:44 +0100

    codage du tableau CO² en dur

--- app/modal.php
+++ app/modal.php
@@ -6,16 +6,51 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="modalCo2Label"><?= $LG['modal_header'] ?></h5>
+        <h5 class="modal-title" id="modalCo2Label">Comparaison des émissions de CO2 pour la fabrication de pieux en béton, en acier et en bois</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <?= $LG['modal_body'] ?>
+        <table>
+    <tr>
+      <th>Pieux béton</th>
+      <th>Pieux acier</th>
+      <th>Pieux bois</th>
+    </tr>
+    <tr>
+      <td>
+      Fabrication béton armé : 367 kg CO2/tonne (Ademe)Extraction de grannulats : 88 kg CO2/tonne (Ademe)
+      </td>
+      <td>
+      Fabrication acier : 3190 kg de CO2/tonne (Ademe).
+      </td>
+      <td>Fabrication bois d’œuvre : 36,7 kg de CO2/tonne (Ademe)</td>
+    </tr>
+    <tr>
+      <td>P béton : 2200 kg/m3</td>
+      <td>P acier : 7 500 kg/m3</td>
+      <td>P bois : 700 kg/m3</td>
+    </tr>
+    <tr>
+      <td>Masse CO2 émise : 319 kg</td>
+      <td>Masse CO2 émise : 8455 kg</td>
+      <td>Masse CO2 émise : 9kg</td>
+    </tr>
+    <tr>
+      <td>Transport grannulats : 10 kg de CO2</td>
+      <td>-</td>
+      <td>Transport de 10 grumes sur 60 km : Véhicule (PTAC) : de 3,51 à 5 tonnes Emission de CO2 à pleine charge : 175 g/km (Ademe) Émission de CO2 : 10,5 kg soit 1 kg de CO2 par grume.</td>
+    </tr>
+    <tr>
+      <td>Masse de CO2 émise : 329 kg</td>
+      <td>Masse CO2 émise : 8 455 kg</td>
+      <td>Masse CO2 émise : 10 kg</td>
+    </tr>
+  </table>
       </div>
       <div class="modal-footer">
-        <p><?= $LG['modal_footer'] ?></p>
+        <p>Les résultats obtenus montrent que la fabrication d’un pieu en bois émet environ 30 fois moins de CO2 que la fabrication d’un pieu en béton et 850 fois moins que celle d’un pieu acier.</p>
       </div>
     </div>
   </div>
