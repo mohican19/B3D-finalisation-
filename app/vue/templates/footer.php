2f09b7dad4f089f04c5c618623d686dba6ec81d8
Noémie <noemielafont19@gmail.com>
2019-03-04 09:16:37 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/vue/templates/footer.php
+++ app/vue/templates/footer.php
@@ -5,17 +5,17 @@ utilisation via include 'app/vue/templates/footer.php';
 
 <?php
 $textFooter1= "B3D, ". date('Y') .". Tous droits réservés";
-$textFooter2 = "Site développé par:";
+$textFooter2 = "Site développé par :";
 $textFooter3 = "Mentions légales";
 ?>
 
 <footer id="footer" class="container-fluid">
 
     <div class="bas-de-page row">
-      <div class="col-lg-1 col-md-1 col-sm-1  m-0 pr-0 d-flex align-items-center">
+      <div class="col-lg-2 col-md-2 m-0 pr-0 d-flex align-items-center">
         <a href="index.php"><img src='images/logo_blanc_100.png'></a>
       </div>
-      <div class="col-lg-3 col-md-8 col-sm-11  pt-1 justify-content-start d-flex align-items-center">
+      <div class="col-lg-3 col-md-3 pt-1 justify-content-start d-flex align-items-center">
         <div class="texte-footer">
             <p><?= $textFooter1 ?></p>
             <a href="index.php"><?= $textFooter3 ?></a>
@@ -24,7 +24,7 @@ $textFooter3 = "Mentions légales";
       <div class="col-lg-4 text-center">
 						
 			</div>
-      <div class="col-lg-2 col-md-2  col-sm-11 justify-content-end d-flex align-items-center">
+      <div class="col-lg-3 col-md-3 justify-content-end d-flex align-items-center">
         <p><?= $textFooter2;?></p>
       </div>
       <div class="col-lg-2 col-md-1 col-sm-1 pl-0 justify-content-start d-flex align-items-center">
