3f1087d5cb411f90e5e27ed800370ad884a21a20
mohican19 <alban.belin@gmail.com>
2019-03-05 10:18:12 +0100

    nettoyage code + responsive

--- app/vue/templates/header.php
+++ app/vue/templates/header.php
@@ -16,84 +16,55 @@ $contactMenu = 'Contact';
 
 <header id="header" class="container-fluid ">
   <div class="en-tete row">
-    <div class="col-lg-1 col-md-3 col-sm-10  d-flex  align-items-center">
+    <div class="col-lg-1 col-md-2 col-sm-10  d-flex  align-items-center">
         <a class="" href="./index.php">
           <img src="images/logo_blanc_100.png" class=""/>
         </a>
     </div>
 
-    <div class="col-lg-8 col-md-8 col-sm-10 justify-content-start d-flex align-items-center">
-
+    <div class="col-lg-8 col-md-2 col-sm-10 justify-content-start d-flex align-items-center">
       <nav id="large-desktop" class="navbar navbar-expand-lg">
-
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
           <ul class="navbar-nav mr-auto">
-
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle <?php if (strpos($_SERVER['REQUEST_URI'], 'index.php?action=produits')) {
-     echo('active');
- } ?>"  href="index.php?action=produits"><?= $produitsMenu; ?></a>
-              <div class="dropdown-menu">
-
+     echo('active'); } ?>"  href="index.php?action=produits"><?= $produitsMenu; ?></a>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                 <div class="nav-panel">
                   <div class="product-area-content text-center">
-                        <a class="product-link" href="index.php?action=produits&id=1">
+                        <a class="product-link dropdown-item" href="index.php?action=produits&id=1">
                         <img src="images/panel-acacia.jpg" alt="produits B3D - acacia">
                         <?= $acaciaSsMenu ?>
                         </a>
-
-                        <a class="product-link" href="index.php?action=produits&id=2">
+                        <a class="product-link dropdown-item" href="index.php?action=produits&id=2">
                         <img src="images/panel-pin.jpg"  alt="produits B3D - pin">
                         <?= $pinSsMenu ?>
                         </a>
-
-                        <a class="product-link" href="index.php?action=produits&id=3">
+                        <a class="product-link dropdown-item" href="index.php?action=produits&id=3">
                         <img src="images/panel-chataigner.jpg" alt="produits B3D - chataignier">
                         <?= $chataignierSsMenu ?>
                         </a>
-
-<!--              <a class="nav-link dropdown-toggle <?php if (strpos($_SERVER['REQUEST_URI'], 'index.php?action=produits')) {echo('active');} ?>"  href="index.php?action=produits" ><?= $produitsMenu; ?>
-              </a>
-                <div class="dropdown-menu">
-                  <div class="nav-panel">
-                    <div class="product-area-content text-center">
-                      <a class="product-link" href="index.php?action=produits&id=1">
-                        <img src="images/panel-acacia.jpg" alt="produits B3D - acacia"><?= $acaciaSsMenu ?>
-                      </a>
-                      <a class="product-link" href="index.php?action=produits&id=2">
-                        <img src="images/panel-pin.jpg"  alt="produits B3D - pin"><?= $pinSsMenu ?>
-                      </a>
-                      <a class="product-link" href="index.php?action=produits&id=3">
-                        <img src="images/panel-chataigner.jpg" alt="produits B3D - chataignier"><?= $chataignierSsMenu ?>
-                      </a>
--->
-                    </div><!-- product-area-content
+                    </div><!-- product-area-content -->
                   </div><!-- nav-panel -->
                 </div>
-
             </li>
-
             <li class="nav-item">
               <a class="nav-link <?php if (strpos($_SERVER['REQUEST_URI'], 'index.php?action=societe')) {echo('active');} ?>" href="index.php?action=societe"><?= $societeMenu ;?>
               </a>
             </li>
-
             <li class="nav-item">
               <a class="nav-link <?php if (strpos($_SERVER['REQUEST_URI'], 'index.php?action=contact')) {echo('active');} ?>" href="index.php?action=contact"><?= $contactMenu ;?>
               </a>
             </li>
-
           </ul>
         </div>
       </nav>
     </div>
     <!-- icones choix langue  -->
-    <div class="col-lg-3 col-md-3 col-sm-10 d-flex justify-content-end align-items-start pt-2 pr-5">
+    <div class="col-lg-3 col-md-8 col-sm-10 d-flex justify-content-end align-items-start pt-2 pr-5">
       <div class="row">
 					<ul class="list-inline">
 						<li class="list-inline-item">
