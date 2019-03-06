6bb214791b7323cbd8f980000f880630afc2e6a2
mohican19 <alban.belin@gmail.com>
2019-03-06 09:02:48 +0100

    debug javascript scroll-up

--- js/js.js
+++ js/js.js
@@ -4,12 +4,67 @@ $('#show-desc').on('click', function(){
         })
 
 // le scroll de la fléche du bas
-/*$(".scroll-up").click(function() {
+$(".scroll-up").click(function() {
   var section = $('#header');
   $("html,body").animate({
   scrollTop: section.offset().top
   }, "slow");
+});
+
+$(window).scroll(function() {
+  var h = window.pageYOffset;
+  if (h > 80) {
+    $(".scroll-up").fadeIn();
+  } else {
+    $(".scroll-up").fadeOut();
+  }
+});
+
+$(window).scroll(function() {
+  var hFooter = $('footer').outerHeight();
+  var hBody = $('body').outerHeight();
+  var hWindow = $(window).outerHeight();
+  var limiteY = hBody - hFooter - hWindow + 50;
+  var h = window.pageYOffset;
+
+  if ( h >= limiteY ) {
+      console.log ("ok");
+    $('.scroll-up').css({ bottom: '205px'}).animate();
+    /*elt.animate({position: 'sticky', bottom: '205'}, 1);*/
+  } else if ( h < limiteY ) {
+     console.log ("non");
+    $('.scroll-up').css({ bottom: '70px'}).animate();
+  }
+});
+
+/*$('footer').ready(function() {
+  console.log ("footer");
+  var h = $(window).scrollTop();
+  var hfooter = $('footer').height();
+  console.log (hfooter);
+  console.log (h);
+  if (h > 90) {
+    console.log ('ok');
+} else { console.log ('non');}
 });*/
+
+//Apparition('.scroll-up');
+//blocage de la fleche au footer
+
+
+/*$(window).scroll(function() {
+  var s = $(window).scrollTop();
+  if (s > 80) {
+    $('.scroll-up').fadeIn();
+  } else {
+    $('.scroll-up').fadeOut();
+  }
+
+  $('.scroll-up').click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 500);
+      return false;
+  });
+}*/
 
 ScrollToTop=function() {
   var s = $(window).scrollTop();
@@ -25,8 +80,9 @@ ScrollToTop=function() {
   });
 }
 
-StopElt=function() {
-            var elt = $('.scroll-up');
+/*StopElt('.scroll-up');
+function StopElt(element) {
+            var elt = $(element);
             var hFooter = $('footer').outerHeight();
             var hBody = $('body').outerHeight();
             var hWindow = $(window).outerHeight();
@@ -42,15 +98,15 @@ StopElt=function() {
             console.log (dTop);
             console.log (dBottom);*/
 
-            if ( h >= limiteY ) {
+          /*if ( h >= limiteY ) {
               console.log ("ok");
-            $('.scroll-up').css('bottom', '200px').animate('slow');
+            $('.scroll-up').css({position: 'sticky', bottom: '205', left: '96%', margin: '5px'});
+            /*elt.animate({position: 'sticky', bottom: '205'}, 1);
           } else if ( h < limiteY ) {
              console.log ("non");
-            $('.scroll-up').css('bottom', '70px');
-            }
-
-}
+            elt.css({position: 'fixed', bottom: '70px'});
+          }
+}*/
 
 StopAnimation=function() {
   $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
@@ -58,8 +114,8 @@ StopAnimation=function() {
   });
 }
 
-$(window).scroll(function() {
+/*$(window).scroll(function() {
   ScrollToTop();
   StopAnimation();
-  StopElt();
-});
+  //StopElt();
+});*/
