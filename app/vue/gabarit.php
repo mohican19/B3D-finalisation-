7acd2f9be8ef2af6b6c80d5ef0f6d329eeb2098d
JBA <jbacorrtech@gmail.com>
2019-03-07 09:28:59 +0100

    Correction fausse manip gabarit

--- app/vue/gabarit.php
+++ app/vue/gabarit.php
@@ -3,7 +3,6 @@
     <head>
         <title><?= $titre ?></title>
         <meta charset="utf-8">
-        <base href="<?= $racineWeb ?>">
       	<meta http-equiv="X-UA-Compatible" content="IE=edge">
       	<meta name="viewport" content="width=device-width, initial-scale=1">
       	<link href="lib/bootstrap-4.1/css/bootstrap.min.css" rel="stylesheet">
