0a898bc15c80e69145ec423df7041633ec8e6e14
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-03-07 21:32:09 +0100

    footer.php et un poil de modif sur master.php lié au footer

--- app/vue/templates/footer.php
+++ app/vue/templates/footer.php
@@ -1,3 +1,4 @@
+
 <!------------------------------
 création du footer,
 utilisation via include 'app/vue/templates/footer.php';
@@ -5,25 +6,38 @@ utilisation via include 'app/vue/templates/footer.php';
 <footer id="footer" class="container-fluid">
 
     <div class="bas-de-page row">
-      <div class="col-lg-2 col-md-2 m-0 pr-0 d-flex align-items-center">
-        <a href="index.php"><img src='<?= $footerImgs[1]['image']; ?>' alt="<?= $footerImgs[1][$langue]; ?>"></a>
-      </div>
-      <div class="col-lg-3 col-md-3 pt-1 justify-content-start d-flex align-items-center">
-        <div class="texte-footer">
-            <p><?= 'B3D, '.date('Y').' '.$footer[0][$langue]; ?></p>
-            <a href="index.php?action=legales"><?= $footer[2][$langue]; ?></a>
-        </div>
-      </div>
-      <div class="col-lg-2 text-center">
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 m-0 pr-0 d-flex align-items-center">
 
-			</div>
-      <div class="col-lg-3 col-md-3 justify-content-end d-flex align-items-center">
-        <p><?= $footer[1][$langue];?></p>
+            <div class="col-lg-2 col-md-5 col-sm-2 col-xs-3 justify-content-start d-flex align-items-center">
+            <a href="index.php"><img src='<?= $footerImgs[1]['image']; ?>' alt="<?= $footerImgs[1][$langue]; ?>" width="85%"></a>
+           </div>
+
+            <div class="col-lg-10 col-md-7 col-sm-10 col-xs-9 mx-1 justify-content-start d-flex align-items-center">
+                <div class="texte-footer">
+                    <p><?= 'B3D, '.date('Y').' '.$footer[0][$langue]; ?>
+                    <br>
+                    <a href="index.php?action=legales"><?= $footer[2][$langue]; ?></a></p>
+                </div>
+            </div>
+
       </div>
-      <div class="col-lg-2 col-md-1 col-sm-1 pl-0 justify-content-start d-flex align-items-center">
-        <a href="https://corrtech.fr/"><img src='<?= $footerImgs[0]['image']; ?>' alt="<?= $footerImgs[0][$langue]; ?>" width="60%"></a>
+
+
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 m-0 pr-0 justify-content-end d-flex align-items-center">
+
+        <div class="col-lg-8 col-md-7 col-sm-10 col-xs-9 justify-content-end d-flex align-items-center">
+          <div class="texte-footer">
+            <p><?= $footer[1][$langue];?></p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-5 col-sm-2 col-xs-3 justify-content-end d-flex align-items-center">
+          <a href="https://corrtech.fr/"><img src='<?= $footerImgs[0]['image']; ?>' alt="<?= $footerImgs[0][$langue]; ?>" width="70%"></a>
+        </div>
+
       </div>
     </div>
+
 </footer>
 <div>
 <a id ="" class="scroll-up" href="#header" ><i class=" far fa-arrow-alt-circle-up fa-2x" ></i>
