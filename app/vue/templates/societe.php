d55b1873706612f62d09299f1bbd8b2a95e17081
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-03-08 14:46:53 +0100

    Produits.php modifs responsive

--- app/vue/templates/societe.php
+++ app/vue/templates/societe.php
@@ -8,19 +8,19 @@
       <div class="row justify-content-around mb-5">
     <?php endif; ?>
     <?php if (($i % 2) == 1): ?>
-      <div class="col-md-4">
+      <div class="col-md-5 d-none d-md-flex justify-content-start mx-0 px-0">
         <img class="img-fluid" alt="<?= $img[$i][$langue] ?>" src="<?= $img[$i]['image'] ?>">
       </div>
     <?php endif; ?>
-    <div class="col-md-7 bloc-texte">
+    <div class="col-md-6 bloc-texte mx-0 px-0">
       <h3 class="titre-de-bloc"><?= $item[(2*$i)][$langue] ?></h3>
       <p class="contenu-bloc-texte"><?= $item[(2*$i+1)][$langue] ?></p>
     </div>
   <?php if (($i % 2) == 0): ?>
-    <div class="col-md-4">
+    <div class="col-md-5 d-none d-md-flex justify-content-end mx-0 px-0">
       <img class="img-fluid" alt="<?= $img[$i][$langue] ?>" src="<?= $img[$i]['image'] ?>">
     </div>
   <?php endif; ?>
   </div>
-  <?php  endfor; ?>
+  <?php endfor; ?>
 </section>
