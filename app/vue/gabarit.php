66992bc3a9d5b6e06141e080f43bfb63bfab21ef
Eric Monpeurt <ericmonpeurt@gmail.com>
2019-03-15 09:10:42 +0100

    Ajout lien favicon

--- app/vue/gabarit.php
+++ app/vue/gabarit.php
@@ -7,6 +7,7 @@
       	<meta name="viewport" content="width=device-width, initial-scale=1">
       	<link href="lib/bootstrap-4.1/css/bootstrap.min.css" rel="stylesheet">
       	<script src="lib/bootstrap-4.1/js/bootstrap.min.js"></script>
+        <link rel="icon" href="images/favicon.jpg" />
       	<script src="lib/jquery-3.3.1.min.js"></script>
       	<script src="lib/bootstrap-4.1/js/bootstrap.bundle.min.js"></script>
       	<link href="lib/fontawesome-free-5.5.0-web/css/all.css" rel="stylesheet">
